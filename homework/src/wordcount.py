82aed1374e176e188817f22058939a5b8288c98e
Samuel Reyes <sreyesn@unal.edu.co>
2025-05-03 10:53:54 -0500

    Importaciones corregidas

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,11 +1,11 @@
 # obtain a list of files in the input directory
 import os
 
-from homework.src._internals.count_words import count_words
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.split_in_words import split_in_words
-from homework.src._internals.write_count_words import write_count_words
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_in_words import split_in_words
+from ._internals.write_count_words import write_count_words
 
 
 def main():
